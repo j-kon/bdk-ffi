0f34b6e63aec0dbd878723eff3946fd6e300e48f
j-kon <jaykon4me@gmail.com>
2026-03-26 12:20:47 +0100

    test: refine wallet constructor flows

--- bdk-ffi/src/tests/wallet.rs
+++ bdk-ffi/src/tests/wallet.rs
@@ -9,6 +9,7 @@ use std::sync::Arc;
 
 const EXTERNAL_DESCRIPTOR: &str = "wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/1h/0/*)";
 const INTERNAL_DESCRIPTOR: &str = "wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/1h/1/*)";
+const TWO_PATH_DESCRIPTOR: &str = "wpkh([9a6a2580/84'/1'/0']tpubDDnGNapGEY6AZAdQbfRJgMg9fvz8pUBrLwvyvUqEgcUfgzM6zc2eVK4vY9x9L5FJWdX8WumXuLEDV5zDZnTfbn87vLe9XceCFwTu9so9Kks/<0;1>/*)";
 const EXPECTED_FIRST_ADDRESS: &str = "tb1qhjys9wxlfykmte7ftryptx975uqgd6kcm6a7z4";
 
 fn external_descriptor() -> Arc<Descriptor> {
@@ -17,6 +18,10 @@ fn external_descriptor() -> Arc<Descriptor> {
 
 fn internal_descriptor() -> Arc<Descriptor> {
     Arc::new(Descriptor::new(INTERNAL_DESCRIPTOR.to_string(), Network::Signet).unwrap())
+}
+
+fn two_path_descriptor() -> Arc<Descriptor> {
+    Arc::new(Descriptor::new(TWO_PATH_DESCRIPTOR.to_string(), Network::Signet).unwrap())
 }
 
 fn build_wallet() -> Wallet {
@@ -58,9 +63,25 @@ fn test_reveal_next_address() {
 
 #[test]
 fn test_create_single_wallet() {
-    let descriptor = external_descriptor();
     let wallet = Wallet::create_single(
-        descriptor.clone(),
+        external_descriptor(),
+        Network::Signet,
+        Arc::new(Persister::new_in_memory().unwrap()),
+        25,
+    )
+    .unwrap();
+
+    let address_info = wallet.reveal_next_address(KeychainKind::External);
+
+    assert_eq!(address_info.index, 0);
+    assert_eq!(address_info.keychain, KeychainKind::External);
+    assert_eq!(address_info.address.to_string(), EXPECTED_FIRST_ADDRESS);
+}
+
+#[test]
+fn test_create_two_path_wallet() {
+    let wallet = Wallet::create_from_two_path_descriptor(
+        two_path_descriptor(),
         Network::Signet,
         Arc::new(Persister::new_in_memory().unwrap()),
         25,
@@ -68,16 +89,19 @@ fn test_create_single_wallet() {
     .unwrap();
 
     assert_eq!(wallet.derivation_index(KeychainKind::External), None);
+    assert_eq!(wallet.derivation_index(KeychainKind::Internal), None);
 
-    let address_info = wallet.reveal_next_address(KeychainKind::External);
+    let external_address = wallet.reveal_next_address(KeychainKind::External);
+    let internal_address = wallet.reveal_next_address(KeychainKind::Internal);
 
-    assert_eq!(address_info.index, 0);
-    assert_eq!(address_info.keychain, KeychainKind::External);
-    assert_eq!(address_info.address.to_string(), EXPECTED_FIRST_ADDRESS);
-    assert_eq!(wallet.derivation_index(KeychainKind::External), Some(0));
-    assert_eq!(wallet.next_derivation_index(KeychainKind::External), 1);
-    assert_eq!(
-        wallet.public_descriptor(KeychainKind::External),
-        descriptor.to_string()
+    assert_eq!(external_address.index, 0);
+    assert_eq!(external_address.keychain, KeychainKind::External);
+    assert_eq!(internal_address.index, 0);
+    assert_eq!(internal_address.keychain, KeychainKind::Internal);
+    assert_ne!(
+        external_address.address.to_string(),
+        internal_address.address.to_string()
     );
+    assert_eq!(wallet.derivation_index(KeychainKind::External), Some(0));
+    assert_eq!(wallet.derivation_index(KeychainKind::Internal), Some(0));
 }
